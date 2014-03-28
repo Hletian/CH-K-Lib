8e2475bba98c457e609a5859158a2cf37fd2723b
yandld <yandld@126.com>
2014-03-28 21:59:33 +0800

    code indent

--- Projects/K60开发板(CHK60EVB)例程/23-CANBus(CAN)/src/main.c
+++ Projects/K60开发板(CHK60EVB)例程/23-CANBus(CAN)/src/main.c
@@ -58,34 +58,34 @@ CAN_RxMsgTypeDef CAN_RxMsg1;
 
 int main(void)
 {
-	  uint8_t i;
-	  uint32_t cnt = 0;
+    uint8_t i;
+    uint32_t cnt = 0;
     CAN_InitTypeDef CAN_InitStruct1;
     //使用CAN时 CoreClock 必须是96M
     SystemClockSetup(ClockSource_EX50M,CoreClock_96M);
     DelayInit();
     LED_Init(LED_PinLookup_CHK60EVB, kNumOfLED);
     UART_DebugPortInit(UART4_RX_C14_TX_C15, 115200);
-	  DisplayCPUInfo();
-	  //配置CAN初始化信息
+    DisplayCPUInfo();
+    //配置CAN初始化信息
     CAN_InitStruct1.CANxMap = CAN1_TX_PE24_RX_PE25;      //PE24 PE25引脚
     CAN_InitStruct1.CAN_BaudRateSelect = CAN_SPEED_125K; //125K 波特率
-	  CAN_InitStruct1.FilterEnable = ENABLE;              //不使用过滤ID功能
-	  CAN_Init(&CAN_InitStruct1);
+    CAN_InitStruct1.FilterEnable = ENABLE;              //不使用过滤ID功能
+    CAN_Init(&CAN_InitStruct1);
 	  
-	  //配置接收信息
-	  CAN_RxMsg1.MBIndex = CAN_MB1;                  //接收邮箱使用CAN_MB1
-	  CAN_RxMsg1.DLC = 8;                            //最多接收8个字节 一帧
+    //配置接收信息
+    CAN_RxMsg1.MBIndex = CAN_MB1;                  //接收邮箱使用CAN_MB1
+    CAN_RxMsg1.DLC = 8;                            //最多接收8个字节 一帧
     CAN_RxMsg1.Id = 7;                             //接收ID
-	  CAN_EnableReceiveMB(CAN1, &CAN_RxMsg1);        //使能接收邮箱
-	  //配置发送数据
+    CAN_EnableReceiveMB(CAN1, &CAN_RxMsg1);        //使能接收邮箱
+    //配置发送数据
     CAN_TxMsg1.Id = 10;                          //发送ID 10
     CAN_TxMsg1.DLC = 8;                          //每帧8字节
     CAN_TxMsg1.IDE = CAN_IDE_Standard;           //标准数据帧
     CAN_TxMsg1.RTR = CAN_RTR_Data;               //数据帧
     CAN_TxMsg1.MBIndex = CAN_MB2;                //使用邮箱0
 		
-	  //写入测试数据  
+    //写入测试数据  
     memcpy(CAN_TxMsg1.Data, "12345678", CAN_TxMsg1.DLC);
     while(1) 
     { 
@@ -96,12 +96,12 @@ int main(void)
             UART_printf("Data: ");
             for(i = 0; i < CAN_RxMsg1.DLC; i++)
             {
-						    UART_printf("0x%x ", CAN_RxMsg1.Data[i]);
+                UART_printf("0x%x ", CAN_RxMsg1.Data[i]);
             }
-						UART_printf("\r\n");
+            UART_printf("\r\n");
         }
         cnt++; 
-				//发送
+        //发送
         if(cnt == 1000000)
         {
             CAN_Transmit(CAN1, &CAN_TxMsg1);
